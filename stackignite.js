7b4d6fbbd2f08c5dd9f85a779d999aed48ebd7c6
beothorn <lucass@gmail.com>
2023-11-19 00:38:11 +0100

    Recursive solution so I can zoom later

--- stackignite.js
+++ stackignite.js
@@ -1,3 +1,5 @@
+const lineHeight = 20;
+
 function drawBar(ctx, x, y, width, height, clearWidth, color, text) {
     ctx.beginPath();
     ctx.rect(x, y, width, height);
@@ -12,25 +14,128 @@ function drawBar(ctx, x, y, width, height, clearWidth, color, text) {
     ctx.clearRect(x + width, y, clearWidth - x, height);
 }
 
-function render(canvas, data) {
+function renderByCall(canvas, data) {
     var canvasWidth = canvas.offsetWidth;
     var canvasHeight = canvas.offsetHeight;
+    console.log({
+        canvasWidth,
+        canvasHeight
+    });
     const height = 20;
     const ctx = canvas.getContext("2d");
     const size = canvasWidth;
-    const text = "InplaceQuickSort.sort(int[] = [10, 14, 7, 11, 8, 5, 15, 12, 1, 9, 3, 4, 2, 13, 6], int = 0, int = 14) => void";
+    const text = data.name;
     drawBar(ctx, 0, canvasHeight - height, size, height, canvasWidth, "red", text);
-    drawBar(ctx, 0, canvasHeight - (height * 2), (canvasWidth/2), height, canvasWidth, "green", text);
-    drawBar(ctx, (canvasWidth/2), canvasHeight - (height * 2), (canvasWidth/2), height, canvasWidth, "blue", text);
+    let children = data.children;
+    let currentLine = 1;
+    const startTimestamp = data.entryTime;
+    const endTimestamp = data.exitTime;
+    const executionTime = endTimestamp - startTimestamp;
+    while (children.length > 0) {
+        currentLine++;
+        let currentY = canvasHeight - (height * currentLine);
+        let newChildren = [];
+        for (let i = 0; i < children.length; i++) {
+            const child = children[i];
+            const childEntryTime = child.entryTime;
+            const childExitTime = child.exitTime;
+            const x = ((childEntryTime - startTimestamp) / executionTime) * canvasWidth;
+            let childWidth = ((childExitTime - childEntryTime) / executionTime) * canvasWidth;
+            if (childWidth === 0){ // Less then 1 pixel, skip
+                continue;
+            }
+            drawBar(ctx, x, currentY, childWidth, height, canvasWidth, "green", child.name);
+            if(child.children){
+                newChildren = newChildren.concat(child.children);
+            }
+        }
+        children = newChildren;
+    }
+}
+
+function renderChildrenByTimestamp(
+    ctx, 
+    canvasWidth, 
+    canvasHeight, 
+    startX,
+    spanWidth,
+    startTimestamp, 
+    executionTime,  
+    children, 
+    currentLine
+){
+    if(!children || children.length === 0) return;
+
+    let currentY = canvasHeight - (lineHeight * currentLine);
+    for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        const childEntryTime = child.entryTime;
+        const childExitTime = child.exitTime;
+        const childExecutionTime = childExitTime - childEntryTime;
+
+        const x = startX +  (((childEntryTime - startTimestamp) / executionTime) * spanWidth);
+        let childWidth = (childExecutionTime / executionTime) * spanWidth;
+        if (childWidth === 0){ // Less then 1 pixel, skip
+            continue;
+        }
+        drawBar(
+            ctx, 
+            x, 
+            currentY, 
+            childWidth, 
+            lineHeight, 
+            canvasWidth, 
+            "green", 
+            child.name
+        );
+
+        renderChildrenByTimestamp(
+            ctx,  
+            canvasWidth, 
+            canvasHeight,
+            x, 
+            childWidth,
+            childEntryTime, 
+            childExecutionTime, 
+            child.children, 
+            currentLine + 1
+        );
+    }
+    
+}
+
+function renderByTimestamp(canvas, data) {
+    var canvasWidth = canvas.offsetWidth;
+    var canvasHeight = canvas.offsetHeight;
+
+    const ctx = canvas.getContext("2d");
+    console.log({
+        canvasWidth,
+        canvasHeight,
+        h: ctx.height
+    });
+    const startTimestamp = data.entryTime;
+    const endTimestamp = data.exitTime;
+    const executionTime = endTimestamp - startTimestamp;
+    renderChildrenByTimestamp(
+        ctx, 
+        canvasWidth, 
+        canvasHeight,
+        0,
+        canvasWidth,
+        startTimestamp, 
+        executionTime, 
+        [data], 
+        0
+    );
 }
 
 (() => {
     const canvas = document.getElementById("flamegraph");
-    render(canvas, data);
+    renderByTimestamp(canvas, data);
 })();
 
 function resizeCanvas() {
-    console.log("resize");
     const canvas = document.getElementById("flamegraph");
-    render(canvas, data);
+    renderByTimestamp(canvas, data);
 }
